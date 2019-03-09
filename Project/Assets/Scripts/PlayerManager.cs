20ec66daeac80628767270a76cbcdafc6a854e9e
BananaFalls <BananaFallsYT@gmail.com>
2019-03-09 11:52:09 -0800

    Added project

---
+++ Project/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float speed;
+    public GameObject bulletPrefab;
+    public Transform shootLoc;
+    // Update is called once per frame
+    void Update()
+    {
+        FaceMouse();
+        Shoot();
+    }
+
+    
+
+    void Shoot()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, shootLoc.position, shootLoc.rotation);
+        }
+
+    }
+
+    void FaceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y
+            );
+
+        transform.up = direction;
+
+    }
+}
