fb66ceb5a5981b198fe5f690446fb9aec81bc603
BananaFalls <BananaFallsYT@gmail.com>
2019-03-09 17:32:49 -0800

    Added random spawning
    This is in preperation for adding proper asteroids, white cubes will spawn randomly at the top when you press W.

---
+++ Project/Assets/Scripts/Enemies.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+
+    public float xMin = -6f;
+    public float xMax = 6f;
+
+    public float yMin = 3f;
+    public float yMax = 3.5f;
+
+    Vector2 spawnPoint;
+
+    //Asteroids
+
+    public GameObject asteroidOne;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Vector2 spawnPoint = new Vector2(Random.Range(xMin, xMax), Random.Range(yMin, yMax));
+
+        Instantiate(asteroidOne, spawnPoint, transform.rotation);
+    }
+}
