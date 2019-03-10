61a6cf1f090487db6ab7f8f6a44d80dedf4e7a7f
BananaFalls <BananaFallsYT@gmail.com>
2019-03-10 13:30:26 -0700

    Started asteroid movement

--- Project/Assets/Scripts/Enemies.cs
+++ Project/Assets/Scripts/Enemies.cs
@@ -25,6 +25,8 @@ public class Enemies : MonoBehaviour
     float yMinL = 3f;
     float yMaxL = -3.5f;
 
+    public float speed = 5;
+    float side;
     Vector2 spawnPoint;
 
     //Asteroids
@@ -35,20 +37,35 @@ public class Enemies : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
+            side = Random.Range(1, 2);
+            print(side);
             SpawnEnemy();
         }
     }
 
     void SpawnEnemy()
     {
+
         Vector2 spawnPointTop = new Vector2(Random.Range(xMinT, xMaxT), Random.Range(yMinT, yMaxT));
         Vector2 spawnPointBottom = new Vector2(Random.Range(xMinB, xMaxB), Random.Range(yMinB, yMaxB));
         Vector2 spawnPointRight = new Vector2(Random.Range(xMinR, xMaxR), Random.Range(yMinR, yMaxR));
         Vector2 spawnPointLeft = new Vector2(Random.Range(xMinL, xMaxL), Random.Range(yMinL, yMaxL));
 
-        Instantiate(asteroidOne, spawnPointTop, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
-        Instantiate(asteroidOne, spawnPointBottom, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
-        Instantiate(asteroidOne, spawnPointRight, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
-        Instantiate(asteroidOne, spawnPointLeft, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+        if(side == 1)
+        {
+            Instantiate(asteroidOne, spawnPointTop, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));   
+        }
+        else if(side == 2)
+        {
+            Instantiate(asteroidOne, spawnPointBottom, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+        }
+        else if(side == 3)
+        {
+            Instantiate(asteroidOne, spawnPointRight, Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+        }
+        else if(side == 4)
+        {
+            Instantiate(asteroidOne, spawnPointLeft, Quaternion.Euler(new Vector3(0, 0, Random.Range(-90, 35))));
+        }
     }
 }
---
+++ Project/Assets/Scripts/EnemyAddForce.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAddForce : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(-Vector2.up * speed * Time.deltaTime);
+    }
+}
