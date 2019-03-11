264831b9f04b628a5e0f80aef722206e0ef0cbd6
BananaFalls <BananaFallsYT@gmail.com>
2019-03-10 20:23:11 -0700

    Inward asteroids and subfolders
    + Added subfolders in the scripts folder for organisation.
    * Asteroids now move inward only instead of whereever the hell they want.

---
+++ Project/Assets/Scripts/Enemy/EnemyTeleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTeleport : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("entered");
+        if (col.gameObject.tag == "TeleportEnemy")
+        {
+            transform.position = gameObject.transform.position * -1;
+        }
+
+    }
+
+}
